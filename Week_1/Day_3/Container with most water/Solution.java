bb8455f7682096546834b15f89edcba6d960dbc4
Shubham Chand <shubhamchand76188@gmail.com>
2026-06-05 10:08:03 +0530

    Implement maxArea method for water container problem

---
+++ Week_1/Day_3/Container with most water/Solution.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+        int maxArea = 0;
+        
+        while (left < right) {
+            int length = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = length * width;
+            maxArea = Math.max(maxArea, area);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
