d8aef4d5378177938dc3e30f5dbf14fec621b5cb
Shubham Chand <shubhamchand76188@gmail.com>
2026-06-25 15:41:42 +0530

    Implement Reverse Polish Notation evaluation

---
+++ Week_3/Day_4/Evaluate reverse polish notation/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        String operators = "+-*/";
+
+        for (String token : tokens) {
+            if (operators.indexOf(token) != -1) {
+                int d2 = st.pop();
+                int d1 = st.pop();
+
+                int res = 0;
+
+                if (token.equals("+"))
+                    res = d1 + d2;
+                else if (token.equals("-"))
+                    res = d1 - d2;
+                else if (token.equals("*"))
+                    res = d1 * d2;
+                else if (token.equals("/"))
+                    res = d1 / d2;
+
+                st.push(res);
+            } else {
+                st.push(Integer.parseInt(token));
+            }
+        }
+
+
+        return st.peek();
+    }
+}
