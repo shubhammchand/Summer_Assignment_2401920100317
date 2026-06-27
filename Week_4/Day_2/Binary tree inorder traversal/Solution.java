03804f92d16d0f908200701b994f15d01267407c
Shubham Chand <shubhamchand76188@gmail.com>
2026-06-27 23:01:22 +0530

    Implement inorder traversal for binary tree

---
+++ Week_4/Day_2/Binary tree inorder traversal/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        List<Integer> l = new ArrayList<Integer>();
+
+        while (true) {
+            if (root != null) {
+                s.push(root);
+                root = root.left;
+            } else {
+                if (s.isEmpty()) {
+                    break;
+                }
+                root = s.pop();
+                l.add(root.val);
+                root = root.right;
+            }
+        }
+
+        return l;
+    }
+}
